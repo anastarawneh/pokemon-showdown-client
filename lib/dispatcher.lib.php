e46c94f9af1f656fdea42ad1d2bc3decd8980cb7
anastarawneh <anastarawneh2001@icloud.com>
2023-07-14 03:58:22 +0300

    Update dispatcher.lib.php

--- lib/dispatcher.lib.php
+++ lib/dispatcher.lib.php
@@ -425,6 +425,8 @@ class DefaultActionHandler {
 			return;
 		}
 		if (
+				// the server must be registered
+				// !$server ||
 				// the server must send all the required values
 				!isset($reqData['id']) ||
 				!isset($reqData['format']) ||
