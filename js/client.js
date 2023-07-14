de74c38d088388c8cfe93f605aad1f0587822ffa
anastarawneh <anastarawneh2001@icloud.com>
2023-07-14 04:07:12 +0300

    Update client.js

--- js/client.js
+++ js/client.js
@@ -1388,7 +1388,7 @@ function toId() {
 			var serverid = Config.server.id && toID(Config.server.id.split(':')[0]);
 			var silent = data.silent;
 			if (serverid && serverid !== 'showdown') id = serverid + '-' + id;
-			console.log('http://' + Config.routes.client + '/action.php');
+			console.log(id);
 			$.post('http://' + Config.routes.client + '/action.php', {
 				act: 'uploadreplay',
 				log: data.log,
